34d10b95ef9d2be4cd0158e7a4bc6e711562decb
Andreas Kähäri <andreas.kahari@nbis.se>
2025-09-26 15:22:17 +0200

    fix: Restore into the correct database

--- initdb.d/02-restore-db.sh
+++ initdb.d/02-restore-db.sh
@@ -3,8 +3,8 @@
 # This script will restore the database from a dump file if a file named
 # "database.dump" is found in the same directory as this script.
 #
-# It is assumed that the application usernames and passwords have not
-# changed since the dump was created.
+# It is assumed that the application usernames have not changed since
+# the dump was created.
 
 set -u
 
@@ -13,5 +13,8 @@ set -u
 db_dump_file="$(dirname "$0")/database.dump"
 if [ -s "$db_dump_file" ]; then
 	printf 'Restoring database from dump file: %s\n' "$db_dump_file"
-	pg_restore --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" "$db_dump_file"
+	pg_restore \
+		--username "$POSTGRES_USER" \
+		--dbname "$DB_NAME" \
+		"$db_dump_file"
 fi
