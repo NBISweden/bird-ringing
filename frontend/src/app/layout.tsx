6a2595103c77350aaad7a3482e3a9a92245bb8b5
Henrike <henrike.wiemker@t-online.de>
2025-10-15 13:53:06 +0200

    feat: add Bootstrap. Issue #17

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function RootLayout({
   children,
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
         <h1>Welcome to Birdy!</h1>
         <p>The most fantastic place to manage your licenses.</p>
       </div>
+      <div>
+        <button className="btn btn-primary">
+          <i className="bi bi-twitter"></i> Fly with Bootstrap
+        </button>
+      </div>
     </main>
   );
 }
