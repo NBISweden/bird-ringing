dcd0594f375ec7dc9ae487d8b449b9cfec5f2e95
Andreas Kähäri <andreas.kahari@nbis.se>
2025-10-17 13:36:38 +0200

    fix: Always use "export" as output mode

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: process.env.NODE_ENV === "production" ? "export" : undefined,
+	output: "export"
 };
 
 export default nextConfig;
