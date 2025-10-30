5f88543b9b31c48464c16ffa97e49456a74d666c
Alex Aperis <aperisalex@gmail.com>
2025-10-30 15:42:12 +0100

    feat: add health endpoint

--- backend/system/urls.py
+++ backend/system/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SystemInfoView
+from .views import HealthCheckView
 
 urlpatterns = [
     path('info/', SystemInfoView.as_view(), name='system-info'),
+    path('health/', HealthCheckView.as_view(), name='system-info'),
 ]
--- backend/system/views.py
+++ backend/system/views.py
@@ -1,11 +1,14 @@
 import platform
 import time
 from datetime import timedelta
-import django
+from django import get_version
 from django.conf import settings
+from django.core.cache import cache
+from django.db import connection
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 SERVER_START_TIME = time.time()
 
@@ -21,7 +24,7 @@ class SystemInfoView(APIView):
                 "version": "0.0.0",
                 "uptime_seconds": uptime,
                 "uptime": str(timedelta(seconds = uptime)),
-                "django_version": django.get_version(),
+                "django_version": get_version(),
                 "python_version": platform.python_version(),
             },
             "config": {
@@ -30,3 +33,36 @@ class SystemInfoView(APIView):
         }
 
         return Response(info)
+
+class HealthCheckView(APIView):
+
+    def get(self, request):
+        health_status = {
+            "database": self.db_check(),
+            "cache": self.cache_check(),
+        }
+
+        # this is True if all checks returned ok
+        is_ready = set(health_status.values()) == {'ok'}
+
+        return Response(
+            {
+                "status": "ok" if is_ready else "not ok",
+                "checks": health_status
+            },
+            status=status.HTTP_200_OK if is_ready else status.HTTP_503_SERVICE_UNAVAILABLE
+        )
+
+    def db_check(self):
+        try:
+            connection.ensure_connection()
+            return "ok"
+        except Exception:
+            return "fail"
+
+    def cache_check(self):
+        try:
+            cache.set("readiness_check", "ok", timeout=5)
+            return "ok" if cache.get("readiness_check") == "ok" else "fail"
+        except Exception:
+            return "fail"
