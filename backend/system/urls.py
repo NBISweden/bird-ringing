85ecc842b2adf0fc7393f6eb9c8bdb26678437ba
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-03 16:42:45 +0100

    fix: Restore Django endpoints to have slashes at the end

--- backend/system/urls.py
+++ backend/system/urls.py
@@ -3,6 +3,6 @@ from .views import SystemInfoView
 from .views import HealthCheckView
 
 urlpatterns = [
-    path("info", SystemInfoView.as_view(), name="system-info"),
-    path("health", HealthCheckView.as_view(), name="system-info"),
+    path("info/", SystemInfoView.as_view(), name="system-info"),
+    path("health/", HealthCheckView.as_view(), name="system-info"),
 ]
