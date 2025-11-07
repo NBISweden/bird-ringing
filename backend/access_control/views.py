49ea296fdeb45dd42cfc88e5a524f2737d012e22
Alex Aperis <aperisalex@gmail.com>
2025-11-07 01:11:20 +0100

    feat: add group based access control to login endpoint

--- backend/access_control/views.py
+++ backend/access_control/views.py
@@ -2,11 +2,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
-
+from rest_framework import status
 class LoginView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
 
+        if not request.user.groups.filter(name="Bird ringing experts").exists():
+            return Response(
+                {"detail": "You do not have permission to access this resource."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
         return Response({"message": f"Hello, {request.user.username}!"})
