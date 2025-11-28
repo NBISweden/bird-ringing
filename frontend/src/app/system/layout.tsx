f6ddc7abc8f2314cad6df9c4dc9915b57e64d3a8
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-11-28 14:28:49 +0100

    refactor: make sidebar data driven

--- frontend/src/app/system/layout.tsx
+++ frontend/src/app/system/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar, { NavItem } from "@/components/Sidebar";
 import { UserProvider, ReuqireAuth } from "../../components/UserProvider";
 
 export default function SystemLayout({
@@ -6,11 +6,34 @@ export default function SystemLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navItems: NavItem[] = [
+    {
+      type: "item",
+      label: "Dashboard",
+      href: "/system/dashboard/",
+      id: "dashboard",
+      icon: "bi-speedometer"
+    },
+    {
+      type: "item",
+      label: "Licenses",
+      href: "/system/licenses/",
+      id: "licenses",
+      icon: "bi-journal-check"
+    },
+    {
+      type: "item",
+      label: "Ringare",
+      href: "/system/actors/",
+      id: "ringare",
+      icon: "bi-person-lines-fill"
+    }
+  ]
   return (
     <div className="flex-grow-1 d-flex">
       <UserProvider>
         <ReuqireAuth>
-          <Sidebar/>
+          <Sidebar items={navItems}/>
           <main className="flex-grow-1 p-3">
             {children}
           </main>
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,7 +3,13 @@
 import Link from "next/link";
 import { useState } from "react";
 
-export default function Sidebar() {
+export type NavItem = (
+    {type: "item", label: string, href: string, id: string, icon: string} |
+    {type: "separator"} |
+    {type: "heading", label: string}
+)
+
+export default function Sidebar({items}: {items: NavItem[]}) {
     const [collapsed, setCollapsed] = useState(false);
 
     return (
@@ -20,24 +26,23 @@ export default function Sidebar() {
                 <i className={`bi ${collapsed ? "bi-chevron-right" : "bi-chevron-left"} fs-4`}></i>
             </button>
             <ul className="nav flex-column p-3 flex-grow-1">
-                <li className="nav-item">
-                    <Link href="/" className="nav-link d-flex align-items-center">
-                        <i className="bi bi-speedometer fs-5 text-secondary me-2"></i>
-                        {!collapsed && "Dashboard"}
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link href="/" className="nav-link d-flex align-items-center">
-                        <i className="bi bi-journal-check fs-5 text-secondary me-2"></i>
-                        {!collapsed && "Licenses"}
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link href="/" className="nav-link d-flex align-items-center">
-                        <i className="bi bi-person-lines-fill fs-5 text-secondary me-2"></i>
-                        {!collapsed && "Ringers"}
-                    </Link>
-                </li>
+                {items.map((ni, index) => {
+                    if (ni.type === "item") {
+                        const isActive = false;
+                        return (
+                            <li key={index} className="nav-item">
+                                <Link href={ni.href} className="nav-link d-flex align-items-center">
+                                    <i className={`bi ${ni.icon} fs-5 text-secondary me-2`}></i>
+                                    <span className="nav-label">{ni.label}</span>
+                                </Link>
+                            </li>
+                        )
+                    } else if (ni.type === "separator") {
+                        return <li key={index}><hr /></li>
+                    } else if (ni.type === "heading") {
+                        return <li key={index} className="nav-item"><h3 className="fs-5">{ni.label}</h3></li>
+                    }
+                })}
             </ul>
         </nav>
     );
