3ef96088a0f2949b8a4977dd13f61ee12efbdebb
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2026-01-19 16:57:31 +0100

    fix: created license view component- issue #31

--- frontend/src/app/system/licenses/license-view/page.tsx
+++ frontend/src/app/system/licenses/license-view/page.tsx
@@ -8,6 +8,7 @@ import Spinner from "@/components/Spinner";
 import { useClient } from "../../contexts";
 import { convertDateToLocale } from "../../common";
 import { Client } from "../../client";
+import {LicenceView} from "@/components/LicenceView";
 
 async function fetchLicense(
     [client, _ctx, entryId]: [Client, "license", string]
@@ -60,174 +61,8 @@ function LicenseViewInner() {
                 <div className="col-12 col-xl-10 col-xxl-9">
                     <div className="py-3">
                         <h1 className="h2 mb-1">Licens {data.mnr}</h1>
-                        {/* Header */}
-                        <div className="card border-primary">
-                            <div className="card-header">
-                                <div className="row g-2">
-                                    <div className="col-12 col-md-9">
-                                        <div>
-                                            <span className="fst-italic">Giltig mellan </span>
-                                            {data.current.starts_at} <span className="fst-italic">till</span>
-                                            {" "}{data.current.ends_at}
-                                        </div>
-                                    </div>
-                                    <div className="col-12 col-md-3 fw-light">
-                                        <div>{data.current.location || " "}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-body">
-                                <ul className="list-group list-group-flush">
-                                    {data.current.description ? (
-                                        <li className="list-group-item">{data.current.description}</li>
-                                    ) : null}
-                                    <li className="list-group-item ">
-                                        <div className="d-flex align-items-center">
-                                            <div className="me-auto">
-                                                <span className="me-2">Rapportstatus</span>
-                                                <span
-                                                    className="badge rounded-pill border border-primary text-primary text-capitalize">
-                                            {String(data.current.report_status)}
-                                          </span>
-                                            </div>
-                                            <div className="d-flex gap-3 text-muted small">
-                                              <span>
-                                                Skapad {convertDateToLocale(data.current.created_at)}
-                                              </span>
-                                              <span>
-                                                Uppdaterad {convertDateToLocale(data.current.updated_at)}
-                                              </span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
-                    {/* Permissions */}
-                    <div className="py-3">
-                        <div className="row">
-                            {/* Actors */}
-                            <div className="col-sm-6">
-                                <div className="card border-primary">
-                                    <div className="card-body">
-                                        <h2 className="h3 card-title">Märkare/hjälpare</h2>
-                                        {data.current.actors?.length ? (
-                                            <ul className="list-group list-group-flush">
-                                                {data.current.actors.map((rel, i) => (
-                                                    <li className="list-group-item py-3" key={i}>
-                                                        <div className="row align-items-center g-2">
-                                                            <div className="col-12 col-md-3 fw-semibold text-capitalize">
-                                                                {rel.role}
-                                                            </div>
-                                                            <div className="col-12 col-md-9">
-                                                                <i className="bi bi-person text-primary me-1" />
-                                                                {rel.actor.full_name}({rel.mednr})
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        ) : (
-                                            <p className="text-muted fst-italic">Inga personer kopplade till licensen.</p>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 mb-3 mb-sm-0">
-                                <div className="card border-primary">
-                                    <div className="card-body">
-                                        <h2 className="h3 card-title">Tillstånd</h2>
-                                        <p className="card-text"></p>
-                                        {data.current.permissions?.length ? (
-                                            <ul className="list-group list-group-flush">
-                                                {data.current.permissions.map((p, i) => (
-                                                    <li className="list-group-item" key={i}>
-                                                        <div>
-                                                            <strong>{p.type.name}</strong>
-                                                            {p.type.description ? (<i
-                                                                className="bi bi-info-circle text-primary p-1"
-                                                                role="button"
-                                                                data-bs-toggle="tooltip"
-                                                                data-bs-placement="right"
-                                                                title={p.type.description}
-                                                            />) : (" ") }
-                                                        </div>
-                                                        <div>{p.description || " "}</div>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        ) : (
-                                            <p className="text-muted fst-italic">Inga tillstånd att visa.</p>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-                    {/* Documents */}
-                    <div className="py-3">
-                        <h3 className="h2">Dokument</h3>
-                        {data.current.documents?.length ? (
-                            <ul className="list-group list-group-flush">
-                                {data.current.documents.map((doc, i) => (
-                                    <li className="list-group-item py-3" key={i}>
-                                        <div className="row align-items-center g-2">
-                                            <div className="col-12 col-md-2 fw-semibold text-capitalize">
-                                                {doc.type}
-                                            </div>
-                                            <div className="col-12 col-md-3 text-nowrap">
-                                                <i className="bi bi-person text-primary me-1" />
-                                                {doc.actor}
-                                            </div>
-                                            <div className="col-12 col-md-5">
-                                                <span className="text-muted small me-2">Referens</span>
-                                                <span className="badge rounded-pill border border-primary text-primary">
-                                          {doc.reference}
-                                      </span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <p className="text-muted fst-italic">Inga dokument kopplade till licensen.</p>
-                        )}
-                    </div>
-                    {/* Communication */}
-                    <div className="py-3">
-                        <h3 className="h2">Kommunikation</h3>
-                        {data.current.communication?.length ? (
-                            <ul className="list-group list-group-flush">
-                                {data.current.communication.map((item, i) => (
-                                    <li className="list-group-item py-3" key={i}>
-                                        <div className="row align-items-center g-2">
-                                            <div className="col-12 col-md-2 fw-semibold text-capitalize">
-                                                {item.type}
-                                            </div>
-                                            <div className="col-12 col-md-3 text-nowrap">
-                                                <i className="bi bi-person text-primary me-1" />
-                                                {item.actor}
-                                            </div>
-                                            <div className="col-12 col-md-2">
-                                      <span className="badge rounded-pill border border-primary text-primary text-capitalize">
-                                        {item.status}
-                                      </span>
-                                            </div>
-                                            <div className="col-12 col-md-5">
-                                                <span className="text-muted small me-2">Anteckning</span>
-                                                <span className="fst-italic">“{item.note}”</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <p className="text-muted fst-italic">Ingen kommunikation kopplad till licensen.</p>
-                        )}
-                    </div>
+                    <LicenceView license={data.current}/>
                     {/* History */}
                     <div className="py-3">
                         <h3 className="h2">Historik</h3>
@@ -239,9 +74,6 @@ function LicenseViewInner() {
                                             <div className="row align-items-center g-2">
                                                 <div className="col-12 col-lg-8">
                                                     Giltig under {h.starts_at} till {h.ends_at}
-                                                </div>
-                                                <div className="col-12 col-md-2">
-                                                    <span className="badge text-bg-secondary rounded-pill"> Version {h.version}</span>
                                                 </div>
                                             </div>
 
