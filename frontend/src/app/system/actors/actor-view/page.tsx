dcba5481d6d237b1ec211cbce1bab554f631d67c
Henrike <henrike.wiemker@t-online.de>
2026-01-20 14:18:49 +0100

    feat: add styling to license table. Issue #14

--- frontend/src/app/system/actors/actor-view/page.tsx
+++ frontend/src/app/system/actors/actor-view/page.tsx
@@ -41,15 +41,21 @@ function ActorViewBase() {
     )
   }
 
-  const licenses: ActorLicenseRelation[] = data.current_license_relations;
+  let licenses: ActorLicenseRelation[] = data.current_license_relations
+  licenses.sort((a: ActorLicenseRelation, b: ActorLicenseRelation) => {
+    return new Date(b.ends_at).getTime() - new Date(a.ends_at).getTime()
+  } );
+  
   const roles = new Set<Role>(licenses.map(l => l.role));
-  const get_license_status = (license: ActorLicenseRelation): string => {
+
+  const is_license_active = (license: ActorLicenseRelation): boolean => {
     const today = new Date()
+    const startDate = new Date(license.starts_at)
     const endDate = new Date(license.ends_at)
-    if (endDate >= today) {
-      return "active"
+    if (startDate <= today && endDate >= today) {
+      return true
     } else {
-      return "inactive"
+      return false
     }
   }
   const get_actor_icon = (type: string): string => {
@@ -78,66 +84,80 @@ function ActorViewBase() {
   
   return (
     <div className="container">
-          <h2>
-            <i className={`bi bi-${get_actor_icon(data.type)} me-3`} />
-            {data.full_name}
-          </h2>
-      <div className="card my-4">
-        <div className="card-header d-flex justify-content-between">
-          <div>
-            <span className="m-0">{Array.from(roles).join(", ")}</span>
-            <i className={`bi bi-${get_gender_icon(data.sex)} ms-1`}/>
+      <div className="row">
+        <h2 className="fw-bold">
+          <i className={`bi bi-${get_actor_icon(data.type)} me-3`} />
+          {data.full_name}
+        </h2>
+        <div className="col-12 col-xl-6">
+          <div className="card my-4">
+            <div className="card-header d-flex justify-content-between">
+              <div>
+                <span className="m-0">{Array.from(roles).join(", ")}</span>
+                <i className={`bi bi-${get_gender_icon(data.sex)} ms-1`}/>
+              </div>
+              <p className="fst-italic m-0">* {convertOnlyDateToLocale(data.birth_date)}</p>
+            </div>
+            <div className="card-body pb-0">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex">
+                  <i className="bi bi-envelope-at-fill me-4"/>
+                  <div className="d-flex flex-column">
+                    <span>{data.email}</span>
+                    {data.alternative_email && <span className="text-muted">{data.alternative_email}</span>}
+                  </div>
+                </li>
+                <li className="list-group-item d-flex">
+                  <i className="bi bi-telephone-fill me-4"/>
+                  <div className="d-flex flex-column">
+                    <span>{data.phone_number1}</span>
+                    {data.phone_number2 && <span className="text-muted">{data.phone_number2}</span>}
+                  </div>
+                </li>
+                <li className="list-group-item d-flex">
+                  <i className="bi bi-house-fill me-4"/>
+                  <div className="d-flex flex-column">
+                    <span>{data.address}</span>
+                    {data.co_address && <span>{data.co_address}</span>}
+                    <span>{data.postal_code} {data.city}</span>
+                    {data.country && <span>{data.country}</span>}
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <p className="text-muted text-end small m-1">
+              Uppdaterad {convertDateToLocale(data.updated_at)}
+            </p>
           </div>
-          <p className="fst-italic m-0">* {convertOnlyDateToLocale(data.birth_date)}</p>
         </div>
-        <div className="card-body pb-0">
+        <div className="col-12 col-xxl-9">
+          <h3 className="pt-4 fw-bold">Licenser</h3>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item d-flex">
-              <i className="bi bi-envelope-at-fill me-4"/>
-              <div className="d-flex flex-column">
-                <span>{data.email}</span>
-                {data.alternative_email && <span className="text-muted">{data.alternative_email}</span>}
-              </div>
-            </li>
-            <li className="list-group-item d-flex">
-              <i className="bi bi-telephone-fill me-4"/>
-              <div className="d-flex flex-column">
-                <span>{data.phone_number1}</span>
-                {data.phone_number2 && <span className="text-muted">{data.phone_number2}</span>}
-              </div>
-            </li>
-            <li className="list-group-item d-flex">
-              <i className="bi bi-house-fill me-4"/>
-              <div className="d-flex flex-column">
-                <span>{data.address}</span>
-                {data.co_address && <span>{data.co_address}</span>}
-                <span>{data.postal_code} {data.city}</span>
-                {data.country && <span>{data.country}</span>}
-              </div>
-            </li>
+            {licenses.map(l => (
+              <li className="list-group-item" key={`${l.mnr}-${l.mednr}`}>
+                <div className="row">
+                  <div className="py-2 col-3 text-nowrap d-flex flex-column justify-content-center">
+                    <span>{l.mnr}-{l.mednr}</span>
+                    <span className="text-secondary small">{l.role}</span>
+                  </div>
+                  <div className="py-2 col-5 d-flex flex-column flex-md-row align-items-center ">
+                    <div>
+                      <p className="m-0 text-end">{l.starts_at}</p>
+                      <p className="m-0"><span className="text-muted small">till</span> {l.ends_at}</p>
+                    </div>
+                    <div className="ms-3">
+                      <span className={`badge rounded-pill ms-2 ${is_license_active(l) ? "text-success-emphasis bg-success-subtle" : "text-dark-emphasis bg-body-secondary"}`}>{is_license_active(l) ? "active" : "inactive"}</span>
+                    </div>
+                  </div>
+                  <div className="py-2 col-4 d-flex align-items-center fw-semibold text-capitalize">{l.communication_type}
+                    <span className="badge rounded-pill text-primary border border-primary ms-2">{l.communication_status}</span>
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
-          <p className="text-muted text-end small m-1">
-            Uppdaterad {convertDateToLocale(data.updated_at)}
-          </p>
       </div>
-      <h3>Licenser</h3>
-      <ul className="list-group list-group-flush">
-      {licenses.map(l => (
-        <li className="list-group-item d-flex flex-row">
-              <div className="py-2 px-4" key={`${l.mnr}-${l.mednr}`}>{l.mnr}</div>
-              <div className="py-2 px-4">{l.mednr}</div>
-              <div className="py-2 px-4">{l.role}</div>
-              <div className="py-2 px-4">{l.version}</div>
-              <div className="py-2 px-4">{l.starts_at}</div>
-              <div className="py-2 px-4">{l.ends_at}</div>
-              <div className="py-2 px-4">{get_license_status(l)}</div>
-              <div className="py-2 px-4">{l.communication_type}</div>
-              <div className="py-2 px-4">{l.communication_status}</div>
-          </li>
-
-      ))}
-      </ul>
     </div>
   )
 }
