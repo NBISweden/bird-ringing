b11151e26dbb1cc03ee50751f0171d5df9a73871
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-02-03 15:00:31 +0100

    fix: restore functionality of getCookie

--- frontend/src/app/system/utils.ts
+++ frontend/src/app/system/utils.ts
@@ -1,10 +1,8 @@
 export function getCookie(name: string) {
-  const entry =
-    document.cookie ||
-    ""
-      .split("; ")
-      .map((s) => s.trim())
-      .find((row) => row.startsWith(name + "="));
+  const entry = (document.cookie || "")
+    .split("; ")
+    .map((s) => s.trim())
+    .find((row) => row.startsWith(name + "="));
 
   return entry ? decodeURIComponent(entry.substring(name.length + 1)) : null;
 }
