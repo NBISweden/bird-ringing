f3c66a59c44cd0d87d22cce99dba7e0dbb51b808
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-02-06 16:31:57 +0100

    fix: allow docker compose override file

--- compose-prod.sh
+++ compose-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -
 
+compose_override=`[ -f docker-compose.override.yml ] && echo "-f docker-compose.override.yml"`
+
 exec env UID="$(id -u)" GID="$(id -g)" \
 	docker compose \
 		-f docker-compose.yml \
+		$compose_override \
 		"$@"
