8aa46edb81be90f07ccc25e3e65b2e1f5ec6d0d7
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-02-09 12:49:59 +0100

    fix: use safer method to add override compose file

--- compose-prod.sh
+++ compose-prod.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -
 
-compose_override=`[ -f docker-compose.override.yml ] && echo "-f docker-compose.override.yml"`
+if [ -f docker-compose.override.yml ]; then
+    set -- -f docker-compose.override.yml "$@"
+fi
 
 exec env UID="$(id -u)" GID="$(id -g)" \
 	docker compose \
 		-f docker-compose.yml \
-		$compose_override \
 		"$@"
