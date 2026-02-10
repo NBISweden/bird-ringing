ae7dc72d7cf6a5995cc9e6a3234e0dc5ddb3b195
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-02-10 13:30:30 +0100

    feat: add support for advanced formatting

--- frontend/src/app/system/internationalization.ts
+++ frontend/src/app/system/internationalization.ts
@@ -1,12 +1,22 @@
 import { useMemo } from "react";
 import { PrimitiveType, useIntl } from "react-intl";
 import { TranslationMap, messages } from "../messages";
+import type { FormatXMLElementFn } from "intl-messageformat";
 
 export type Translation = {
   t: (
     messageId: keyof TranslationMap,
     values?: Record<string, PrimitiveType>,
   ) => string;
+  format: (
+    messageId: keyof TranslationMap,
+    values?: Record<
+      string,
+      | React.ReactNode
+      | PrimitiveType
+      | FormatXMLElementFn<string, React.ReactNode>
+    >,
+  ) => Array<React.ReactNode>;
 };
 
 export function useTranslation() {
@@ -15,6 +25,9 @@ export function useTranslation() {
   const translation = useMemo<Translation>(() => {
     return {
       t: (messageId, values?) => {
+        return intl.formatMessage(messages[messageId], values);
+      },
+      format: (messageId, values?) => {
         return intl.formatMessage(messages[messageId], values);
       },
     };
