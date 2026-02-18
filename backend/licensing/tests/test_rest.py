5147fd7434648921024b7dbd327f0f8dfcdcd3a3
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-02-18 15:43:29 +0100

    fix: improve email tests to also include helpers

--- backend/licensing/tests/test_rest.py
+++ backend/licensing/tests/test_rest.py
@@ -14,6 +14,7 @@ from licensing.models import (
     LicenseRoleChoices,
     DocumentTypeChoices,
     LicenseCommunication,
+    CommunicationStatusChoices,
 )
 from .utils import create_user
 import datetime
@@ -81,6 +82,15 @@ class LicenseDocumentEmailTests(TestCase):
                 created_by=self.user_with_access,
                 updated_by=self.user_with_access,
             )
+        for (actor, lic) in zip(self.actors, reversed(self.licenses)):
+            LicenseRelation.objects.create(
+                actor=actor,
+                license=lic,
+                role=LicenseRoleChoices.HELPER,
+                mednr="H001",
+                created_by=self.user_with_access,
+                updated_by=self.user_with_access,
+            )
 
     def test_fail_when_missing_license_documents(self):
         self._with_access()
@@ -102,8 +112,8 @@ class LicenseDocumentEmailTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             {
-                "messages_sent": 2,
-                "messages_prepared": 2,
+                "messages_sent": 4,
+                "messages_prepared": 4,
                 "failed_messages": [],
             }, 
             response.json(),
@@ -120,8 +130,8 @@ class LicenseDocumentEmailTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             {
-                "messages_sent": 1,
-                "messages_prepared": 1,
+                "messages_sent": 3,
+                "messages_prepared": 3,
                 "failed_messages": [],
             }, 
             response.json(),
@@ -136,43 +146,44 @@ class LicenseDocumentEmailTests(TestCase):
         self.assertEqual(response.status_code, 200)
         today_str = str(datetime.date.today())
 
+        actor_license_combos = list(zip([*self.actors, *self.actors], [*self.licenses, *reversed(self.licenses)]))
         self.assertEqual(
-            [
+            sorted([
                 message.body
                 for message in mail.outbox
-            ],
-            [
+            ]),
+            sorted([
                 f"{lic.sequence.mnr} {actor.full_name} {today_str}  license: {self._license_name(lic, actor)}"
-                for (actor, lic) in zip(self.actors, self.licenses)
-            ],
+                for (actor, lic) in actor_license_combos
+            ]),
             "Make sure that the basic message content follows the given template"
         )
 
         self.assertEqual(
-            [
+            sorted([
                 (alternative.content, alternative.mimetype)
                 for message in mail.outbox
                 for alternative in message.alternatives
-            ],
-            [
+            ]),
+            sorted([
                 (
                     f"<em>{lic.sequence.mnr}</em> <em>{actor.full_name}</em> <em>{today_str}</em><ul><li>license: {self._license_name(lic, actor)}</li></ul>",
                     "text/html"
                 )
-                for (actor, lic) in zip(self.actors, self.licenses)
-            ],
+                for (actor, lic) in actor_license_combos
+            ]),
             "Make sure that the html message follows the given template"
         )
 
         self.assertEqual(
-            [
+            sorted([
                 [(filename, mimetype) for (filename, _data, mimetype) in message.attachments]
                 for message in mail.outbox
-            ],
-            [
+            ], key=lambda a: a[0]),
+            sorted([
                 [(self._license_name(lic, actor), "application/pdf")]
-                for (actor, lic) in zip(self.actors, self.licenses)
-            ],
+                for (actor, lic) in actor_license_combos
+            ], key=lambda a: a[0]),
             "Make sure that the license documents are attached"
         )
     
@@ -212,6 +223,17 @@ class LicenseDocumentEmailTests(TestCase):
                 LicenseCommunication.objects.filter(actor=actor, license=lic).count(),
                 "Make sure that there is exactly one communication object per actor and license combination."
             )
+            communications = LicenseCommunication.objects.order_by("created_at").filter(actor=actor, license=lic)
+            
+            for communication in communications:
+                self.assertEqual(
+                    communication.note,
+                    "E-mail with license sent"
+                )
+                self.assertEqual(
+                    communication.status,
+                    CommunicationStatusChoices.SENT
+                )
 
     
     def _license_name(self, lic, actor):
@@ -219,6 +241,17 @@ class LicenseDocumentEmailTests(TestCase):
 
     def _add_license_documents(self, actors, licenses):
         for (actor, lic) in zip(actors, licenses):
+            LicenseDocument.objects.create(
+                is_current=True,
+                actor=actor,
+                license=lic,
+                reference=self._license_name(lic, actor),
+                data=b"b44df00d",
+                type=DocumentTypeChoices.LICENSE,
+                created_by=self.user_with_access,
+                updated_by=self.user_with_access,
+            )
+        for (actor, lic) in zip(actors, reversed(licenses)):
             LicenseDocument.objects.create(
                 is_current=True,
                 actor=actor,
