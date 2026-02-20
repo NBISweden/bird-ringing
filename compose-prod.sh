9b99babcb0ccace945b55c174b9fae30526d3518
Andreas Kähäri <andreas.kahari@nbis.se>
2026-02-20 10:43:10 +0100

    feat: Introduce separate IDs for build and runtime users
    No change to the Dockerfiles yet.

--- compose-prod.sh
+++ compose-prod.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -
 
 if [ -f docker-compose.override.yml ]; then
-    set -- -f docker-compose.override.yml "$@"
+        # If a docker-compose.override.yml file exists, prepend it to
+        # the arguments list.
+	set -- -f docker-compose.override.yml "$@"
 fi
 
-exec env UID="$(id -u)" GID="$(id -g)" \
-	docker compose \
-		-f docker-compose.yml \
-		"$@"
+# Unless already set, the build and runtime user:group IDs default to
+# 6000:6000 and 6001:6001, respectively.
+export BUILD_ID="${BUILD_ID:-6000:6000}"
+export RUNTIME_ID="${RUNTIME_ID:-6001:6001}"
+
+exec docker compose -f docker-compose.yml "$@"
