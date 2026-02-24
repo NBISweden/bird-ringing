7568d65f05992209a665bb8661b2176a4e79ae28
Andreas Kähäri <andreas.kahari@nbis.se>
2026-02-24 11:12:53 +0100

    feat: Cleanup, and also reuse the backend image for the backend-init service

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,10 +3,18 @@
 # The "backend-init" service will apply any pending migrations and then
 # terminate.
 case $SERVICE_MODE in (*-init)
-	case $SERVICE_MODE in (development*)
-		pip install uv &&
-		uv sync --frozen &&
-		python manage.py makemigrations || exit
+	case $SERVICE_MODE in 
+		development*)
+			pip install uv &&
+			uv sync --frozen &&
+			python manage.py makemigrations || exit
+			;;
+		production*)
+			: # Do nothing.
+			;;
+		*)
+			printf "Unknown SERVICE_MODE: %s\n" "$SERVICE_MODE" >&2
+			exit 1
 	esac
 
 	python manage.py migrate
@@ -16,6 +24,14 @@ esac
 # The main "backend" service runs here.
 
 # In development mode, run the Django development server.
-case $SERVICE_MODE in (development*)
-	exec python manage.py runserver 0.0.0.0:8000
+case $SERVICE_MODE in
+	development*)
+		exec python manage.py runserver 0.0.0.0:8000
+		;;
+	production*)
+		exec gunicorn --bind 0.0.0.0:8000 bird_ringing.wsgi
+		;;
+	*)
+		printf "Unknown SERVICE_MODE: %s\n" "$SERVICE_MODE" >&2
+		exit 1
 esac
