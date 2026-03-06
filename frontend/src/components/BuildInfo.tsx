23282d3e93c61a583810fc9c827f3d03755869c5
Andreas Kähäri <andreas.kahari@nbis.se>
2026-03-06 13:36:25 +0100

    feat: Add BildInfo component

---
+++ frontend/src/components/BuildInfo.tsx
@@ -0,0 +1,24 @@
+export default function BuildInfo() {
+  // Only render if at least one of the env vars is available
+  const hasBuildInfo =
+    process.env.NEXT_PUBLIC_BUILD_DATE ||
+    process.env.NEXT_PUBLIC_BUILD_GIT_COMMIT ||
+    process.env.NEXT_PUBLIC_BUILD_GIT_BRANCH;
+
+  if (!hasBuildInfo) return null;
+
+  return (
+    <div className="build-info">
+    	<h2>Build info</h2>
+        {process.env.NEXT_PUBLIC_BUILD_DATE && (
+          <div>Build time: {process.env.NEXT_PUBLIC_BUILD_DATE}</div>
+        )}
+        {process.env.NEXT_PUBLIC_BUILD_GIT_COMMIT && (
+          <div>Commit: <code>{process.env.NEXT_PUBLIC_BUILD_GIT_COMMIT}</code></div>
+        )}
+        {process.env.NEXT_PUBLIC_BUILD_GIT_BRANCH && (
+          <div>Branch: <code>{process.env.NEXT_PUBLIC_BUILD_GIT_BRANCH}</code></div>
+        )}
+    </div>
+  );
+}
