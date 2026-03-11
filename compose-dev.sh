aa8e6e20725084564a790ac7691c3ccb12ce2c8d
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-03-11 16:24:13 +0100

    fix: make sure that docker compose override is applied last

--- compose-dev.sh
+++ compose-dev.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -
 
+if [ -f docker-compose.override.yml ]; then
+        # If a docker-compose.override.yml file exists, prepend it to
+        # the arguments list.
+	set -- -f docker-compose.override.yml "$@"
+fi
+
 # In development, the build and runtime user:group is the same as the
 # host's user:group, so that files created by the container are owned by
 # the host user.
